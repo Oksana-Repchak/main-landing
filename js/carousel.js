2d88113c5e6444bd563ee48d9d6249fbaebc4209
Oksana Repchak <oksa.repchak@gmail.com>
2021-12-24 15:59:35 +0200

    fix carousel

--- js/carousel.js
+++ js/carousel.js
@@ -3,6 +3,15 @@ const mCarousel = new Carousel(document.querySelector(".carousel"), {
 
   center: false,
 
+  slidesPerPage: 1,
+
+  Navigation: {
+    prevTpl:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 5l-7 7 7 7"/><path d="M4 12h16"/></svg>',
+    nextTpl:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4 12h16"/><path d="M13 5l7 7-7 7"/></svg>',
+  },
+
   on: {
     init: (carousel) => {
       carousel.$index = carousel.$container.querySelector(".carousel_index");
