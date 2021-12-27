d2b1ce2480ca0eb613600613ccd469214ba6128d
Oksana Repchak <oksa.repchak@gmail.com>
2021-12-27 16:31:42 +0200

    add carousel_reviews

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,4 @@
-const mCarousel = new Carousel(document.querySelector(".carousel"), {
+const portfolioCarousel = new Carousel(document.querySelector(".carousel"), {
   Dots: false,
 
   center: false,
@@ -29,3 +29,38 @@ const mCarousel = new Carousel(document.querySelector(".carousel"), {
     },
   },
 });
+
+const reviewsCarousel = new Carousel(
+  document.querySelector(".carousel_reviews"),
+  {
+    Dots: false,
+
+    center: false,
+
+    slidesPerPage: "auto",
+
+    Navigation: {
+      prevTpl:
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 5l-7 7 7 7"/><path d="M4 12h16"/></svg>',
+      nextTpl:
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4 12h16"/><path d="M13 5l7 7-7 7"/></svg>',
+    },
+
+    on: {
+      init: (carousel) => {
+        carousel.$index = carousel.$container.querySelector(".carousel_index");
+        carousel.$count = carousel.$container.querySelector(".carousel_count");
+      },
+      refresh: (carousel) => {
+        if (carousel.$count) {
+          carousel.$count.innerHTML = carousel.pages.length;
+        }
+      },
+      change: (carousel) => {
+        if (carousel.$index) {
+          carousel.$index.innerHTML = carousel.page + 1;
+        }
+      },
+    },
+  }
+);
